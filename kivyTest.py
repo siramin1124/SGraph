ac91db37eebca205b47d375b8355870c905d58f7
Siramin1124 <vortexsoundiwagami@gmail.com>
2020-08-30 21:53:30 +0900

    マウス操作の追加

--- kivyTest.py
+++ kivyTest.py
@@ -1,15 +1,18 @@
 from kivy.app import App
-from kivy.uix.label import Label
-from kivy.uix.button import Button
+#from kivy.uix.label import Label
+#from kivy.uix.button import Button
 from kivy.uix.widget import Widget
 
 
 class Widgets(Widget):
-    pass
+    def on_touch_down(self, touch):
+        print("Click!", touch.spos)
 
-# class kvfile(App):
-#    def build(self):
-#        return Label()
+    def on_touch_up(self, touch):
+        print("Released!", touch.spos)
+
+    def on_touch_move(self, touch):
+        print("Mouce move!", touch)
 
 
 class ButtonApp(App):
