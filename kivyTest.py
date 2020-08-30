88a38cf3245c93a4de31e0dec7906155d89a6d7d
Siramin1124 <vortexsoundiwagami@gmail.com>
2020-08-30 20:19:53 +0900

    FloatLayoutによるボタンの配置

--- kivyTest.py
+++ kivyTest.py
@@ -1,20 +1,13 @@
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.button import Button
-from kivy.uix.widget import Widget
-
-
-class Widgets(Widget):
-    pass
-
-# class kvfile(App):
-#    def build(self):
-#        return Label()
+#from kivy.uix.widget import Widget
+from kivy.uix.floatlayout import FloatLayout
 
 
 class ButtonApp(App):
     def build(self):
-        return Widgets()
+        return FloatLayout()
 
 
 if __name__ == "__main__":
