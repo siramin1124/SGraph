1c6bafadad0ddc99f0b20aa3eda055f85a8ae413
Siramin1124 <vortexsoundiwagami@gmail.com>
2020-08-30 22:26:02 +0900

    簡単なお絵かきソフトの実装

--- DrawInput.py
+++ DrawInput.py
@@ -1,23 +1,26 @@
 from kivy.app import App
 #from kivy.uix.label import Label
-#from kivy.uix.button import Button
+from kivy.uix.button import Button
 from kivy.uix.widget import Widget
+from kivy.graphics import Line
 
 
-class Widgets(Widget):
+class DrawInput(Widget):
     def on_touch_down(self, touch):
         print("Click!", touch.spos)
+        with self.canvas:
+            touch.ud["Sen"] = Line(points=(touch.x, touch.y))
 
     def on_touch_up(self, touch):
-        print("Released!", touch.spos)
+        print(touch.ud["Sen"].points)
 
     def on_touch_move(self, touch):
-        print("Mouce move!", touch)
+        touch.ud["Sen"].points += touch.x, touch.y
 
 
 class ButtonApp(App):
     def build(self):
-        return Widgets()
+        return DrawInput()
 
 
 if __name__ == "__main__":
